0908a61a0723839f989205b870dbcd57cfc8d2cd
charlie_do <charlie.do2604@gmail.com>
2025-01-23 21:57:12 +0100

    nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
-  modules: ["@nuxtjs/google-fonts", '@pinia/nuxt', "@nuxt/image"],
+  modules: ["@nuxtjs/google-fonts", '@pinia/nuxt', "@nuxt/image", '@nuxt/ui'],
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
     scrollBehavior: () => false,
